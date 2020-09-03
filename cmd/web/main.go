d09106aff7075a7e97f584bda60b7c697e65fee8
jinsoo_youn <jinsoo_youn@tmax.co.kr>
2020-09-03 17:11:46 +0900

    [refactor] password pkg 구성

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 
+	"github.com/jinsoo-youn/mail/password"
 	"gopkg.in/gomail.v2"
 	"gopkg.in/yaml.v2"
 )
 
 func main() {
+
+	var key = []byte("tmaxcloudck2-1js")
+	a, _ := password.NewAesCipher(key)
+	pw := a.DecryptString("yFgOiKqHci9M8a29i9HFgx-81mBy8XwUBA==")
+	fmt.Println("real password=", pw)
 
 	content, err := ioutil.ReadFile("./static/config/config.yml")
 	// content, err := ioutil.ReadFile("https://raw.githubusercontent.com/jinsoo-youn/mail/master/config.yaml")
@@ -21,24 +28,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	d := gomail.NewDialer(config.SmtpInfo.SmtpHost, config.SmtpInfo.SmtpPort, config.SmtpInfo.SmtpId, a.DecryptString(config.SmtpInfo.SmtpPw))
 
-	// pw := []byte("52382fbb9879096561ec0a61845c0a394ffdc0396bbe97c45236d7e6dcb93b2fdea879db30")
-
-	// d := gomail.NewDialer("mail.tmax.co.kr", 587, "jinsoo_youn@tmax.co.kr", string(pwr))
-
-	var key = []byte("tmaxcloudck2-1js")
-
-	a, _ := NewAesCipher(key)
-	pw := a.EncryptString("ererer")
-	pasword.NewAesCipher
-	fmt.Println(pw)
-	d := gomail.NewDialer(config.SmtpInfo.SmtpHost, config.SmtpInfo.SmtpPort, config.SmtpInfo.SmtpId, config.SmtpInfo.SmtpPw)
-
-	// d.TLSConfig = &tls.Config{
-	// 	InsecureSkipVerify: true,
-	// 	// ServerName:         "587",
-	// }
-
+	d.TLSConfig = &tls.Config{
+		InsecureSkipVerify: true,
+	}
 	s, err := d.Dial()
 	if err != nil {
 		fmt.Println(err)
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jinsoo-youn/mail
+
+go 1.13
+
+require (
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+	gopkg.in/yaml.v2 v2.3.0
+)
